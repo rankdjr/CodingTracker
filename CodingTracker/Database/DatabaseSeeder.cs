bfbdd91842749c2cc22812de232dfc12b0a99539
rankdjr <douglas.rankjr@gmail.com>
2024-04-15 00:39:50 -0700

    broken

--- CodingTracker/Database/DatabaseSeeder.cs
+++ CodingTracker/Database/DatabaseSeeder.cs
@@ -26,7 +26,7 @@ public class DatabaseSeeder
     /// <param name="numOfSessions">Number of session log entries to create.</param>
     public static void SeedSessions(SessionService sessionService, int numOfSessions)
     {
-        for (int i = 0; i < numOfSessions % 2; i++)
+        for (int i = 0; i < numOfSessions / 2; i++)
         {
             int hours = _random.Next(0, 12); // Random value limited to 12 hours
             int minutes = _random.Next(0, 60); // Random value limited to 59 minutes
@@ -37,7 +37,7 @@ public class DatabaseSeeder
             sessionService.InsertNewSession(newSession);  
         }
 
-        for (int i = 0; i < numOfSessions % 2; i++)
+        for (int i = 0; i < numOfSessions / 2; i++)
         {
             DateTime endTime = DateTime.Today.AddDays(-_random.Next(1, 30));
             DateTime startTime = endTime.AddMinutes(-_random.Next(1,180));
