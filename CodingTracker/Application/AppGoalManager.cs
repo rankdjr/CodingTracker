0edff7064adad929b537e3bf578186416653f983
rankdjr <douglas.rankjr@gmail.com>
2024-04-18 15:01:02 -0700

    skeleton functionality for viewing goal progress complete

--- CodingTracker/Application/AppGoalManager.cs
+++ CodingTracker/Application/AppGoalManager.cs
@@ -34,7 +34,7 @@ public class AppGoalManager
             new SelectionPrompt<string>()
                 .Title("Select an option:")
                 .PageSize(10)
-                .AddChoices(Enum.GetNames(typeof(MainMenuOption))
+                .AddChoices(Enum.GetNames(typeof(ManageGoalsMenuOptions))
                 .Select(Utilities.SplitCamelCase)));
 
         ExecuteSelectedOption(selectedOption);
@@ -45,7 +45,7 @@ public class AppGoalManager
         switch (Enum.Parse<ManageGoalsMenuOptions>(option.Replace(" ", "")))
         {
             case ManageGoalsMenuOptions.ShowProgress:
-                // do stuff
+                HandleShowProgressAction();
                 break;
             case ManageGoalsMenuOptions.SetNewGoal:
                 // do stuff
@@ -56,8 +56,92 @@ public class AppGoalManager
             case ManageGoalsMenuOptions.DeleteGoal:
                 // do stuff
                 break;
+            case ManageGoalsMenuOptions.DeleteAllGoals:
+                DeleteAllGoals();
+                break;
             case ManageGoalsMenuOptions.ReturnToMainMenu:
-                return;
+                _running = false;
+                break;
         }
+    }
+
+    private void HandleShowProgressAction()
+    {
+        ShowGoalProgress();
+        _inputHandler.PauseForContinueInput();
+    }
+
+    private void ShowGoalProgress()
+    {
+        List<CodingGoalModel> codingGoals = _codingGoalDAO.GetAllGoalRecords();
+        List<BreakdownChart> breakdownCharts = BuildCodingGoalsBreakdownCharts(codingGoals);
+        PrintBreakDownCharts(breakdownCharts);
+    }
+
+    private void PrintBreakDownCharts(List<BreakdownChart> charts)
+    {
+        Utilities.PrintNewLines(1);
+        // TODO: Implement header for screen
+
+        foreach (var chart in charts)
+        {
+            string chartHeader = $"Goal Progress: {_codingGoalModel?.Description}"; // TODO: Fix this --> build tuple in breakdownchart function with description and chart
+            var rule = new Rule("[red]Hello[/]");
+            AnsiConsole.Write(rule);
+            Utilities.PrintNewLines(1);
+            AnsiConsole.Write(chart);
+            Utilities.PrintNewLines(3);
+        }
+
+        Utilities.PrintNewLines(2);
+    }
+
+    // TODO: implement --> build tuple in breakdownchart function with description and chart
+    private List<BreakdownChart> BuildCodingGoalsBreakdownCharts(List<CodingGoalModel> codingGoals)
+    {
+        List<BreakdownChart> breakdownCharts = new List<BreakdownChart>();
+
+        foreach (var goal in codingGoals)
+        {
+            string goalDescription = goal.Description!;
+            Color progressBarColor = Color.LightGreen;
+            Color targetDurationBarColor = Color.Grey;
+
+            int progressValue;
+            int totalMinutesTarget;
+
+            goal.GetProgressAsIntervals(out progressValue, out totalMinutesTarget);
+
+            // TODO: Remove after testing
+            Random random = new Random();
+            goal.UpdateProgress(TimeSpan.FromMinutes(random.Next(1, totalMinutesTarget)));
+            goal.GetProgressAsIntervals(out progressValue, out totalMinutesTarget);
+            // end test code
+
+            BreakdownChart breakdownChart = new BreakdownChart()
+                                .Width((int)(Console.WindowWidth * 0.65))
+                                .AddItem("", progressValue, progressBarColor)
+                                .AddItem("", totalMinutesTarget, targetDurationBarColor);
+
+            breakdownCharts.Add(breakdownChart);
+        }
+
+        return breakdownCharts;
+    }
+
+    private void DeleteAllGoals()
+    {
+        bool goalDeletedResult = _codingGoalDAO.DeleteAllGoals();
+        if (goalDeletedResult == true)
+        {
+            Utilities.DisplaySuccessMessage("Goals successfully deleted!");
+        }
+        else
+        {
+            Utilities.DisplayWarningMessage("No goals were deleted. (The table may have been empty).");
+        }
+
+
+        _inputHandler.PauseForContinueInput();
     }
 }
